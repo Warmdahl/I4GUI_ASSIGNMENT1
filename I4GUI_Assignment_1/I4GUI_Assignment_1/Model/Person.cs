716162504016e13db7513b4044531725d4d8c0e4
Maja <maja.sitanala@gmail.com>
2020-03-04 10:17:01 +0100

    Person object class created

--- I4GUI_Assignment_1/I4GUI_Assignment_1/Model/Person.cs
+++ I4GUI_Assignment_1/I4GUI_Assignment_1/Model/Person.cs
@@ -8,6 +8,50 @@ namespace I4GUI_Assignment_1.Model
 {
     class Person
     {
+        private List<double> values_ = new List<double>();
+        private string name_;
+        private double currentDebt_;
 
+        public Person(string name, double startValue)
+        {
+            Name = name;
+            Values.Add(startValue);
+        }
+
+        public List<double> Values
+        {
+            get
+            {
+                return values_;
+            }
+            set
+            {
+                values_ = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name_;
+            }
+            set 
+            {
+                name_ = value;
+            }
+        }
+
+        public double CurrentDebt
+        {
+            get
+            {
+                return currentDebt_;
+            }
+            set
+            {
+                currentDebt_ = value;
+            }
+        }
     }
 }
---
+++ I4GUI_Assignment_1/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace
+
+public class Person
+{
+	public Person()
+	{
+	}
+}
