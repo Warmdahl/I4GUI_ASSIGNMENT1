0de66ddacb3a00028eb2abe13ff44042be49da86
Maja <maja.sitanala@gmail.com>
2020-03-04 10:02:03 +0100

    Added Person class and MVVM class

---
+++ I4GUI_Assignment_1/I4GUI_Assignment_1/Model/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I4GUI_Assignment_1.Model
+{
+    class Person
+    {
+
+    }
+}
